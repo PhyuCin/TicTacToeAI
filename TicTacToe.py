f5fa6a7071b248d2e5c28f92824d96a5e8b46908
Aye Mya Phyu Cin <45021004+PhyuCin@users.noreply.github.com>
2020-08-21 14:40:40 +0800

    testing tkinter window

---
+++ TicTacToe.py
@@ -0,0 +1,11 @@
+#testing
+
+from future.moves import tkinter
+
+window = tkinter.Tk()
+# Code to add widgets will go here...
+
+greeting = tkinter.Label(text="Hello, Tkinter")
+greeting.pack()
+
+window.mainloop()
